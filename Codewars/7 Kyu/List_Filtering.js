da0d6d66652220a8833ffa9681fc8efe05de98a6
Iulian <savin.iulian90@gmail.com>
2018-03-05 02:29:07 +0200

    added 'credit card mask' and 'list filtering'

---
+++ Codewars/7 Kyu/List_Filtering.js
@@ -0,0 +1,26 @@
+// link: https://www.codewars.com/kata/list-filtering/
+
+
+//                           List Filtering
+
+
+// In this kata you will create a function that takes a list of non-negative
+// integers and strings and returns a new list with the strings filtered out.
+
+// Example: filter_list([1,2,'a','b']) == [1,2]
+//          filter_list([1,'a','b',0,15]) == [1,0,15]
+//          filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+
+const filter_list = L=> L.filter(v => typeof v === "number");
+
+// or
+
+function filter_list(l) {
+  for(var i =0, arr =[]; i<l.length;i++) {
+    if(typeof l[i] === 'number') {
+      arr.push(l[i]);
+    }
+  }
+  return arr;
+}
